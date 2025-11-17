08e3633665ed02c9e207688cb6ef2db7bf575ce7
Acacia Edmond <aedmond@macalester.edu>
2025-11-17 13:48:59 -0600

    snake class started

--- src/Snake.java
+++ src/Snake.java
@@ -1,6 +1,20 @@
 import java.util.LinkedList;
 
+
 public class Snake {
-    LinkedList snake; 
+    private LinkedList<Position> body; 
+    public Snake(int row, int col) {
+        body = new LinkedList<>();
+        body.add(new Position(row, col));
+
+    }
+
+    public Position getHead() {
+        return body.getFirst();
+    }
+
+    public LinkedList<Position> getBody() {
+        return body;
+    }
 
 }
