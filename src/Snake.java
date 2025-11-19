45898cb9c6e2059a22c762ee1127c3a5c58a5ca6
Acacia Edmond <aedmond@macalester.edu>
2025-11-19 13:20:59 -0600

    methods in snake class implemented

--- src/Snake.java
+++ src/Snake.java
@@ -2,7 +2,13 @@ import java.util.LinkedList;
 
 
 public class Snake {
+
     private LinkedList<Position> body; 
+    private int dx = 1;
+    private int dy = 0;
+    private boolean growing = false;
+
+
     public Snake(int row, int col) {
         body = new LinkedList<>();
         body.add(new Position(row, col));
@@ -18,11 +24,21 @@ public class Snake {
     }
 
     public void move(){
+        Position head = getHead();
+        Position newHead = new Position(head.row + dy, head.col + dx);
+
+        body.addFirst(newHead);
+
+        if(!growing){
+            body.removeLast();
+        } else {
+            growing = false;
+        }
 
     }
 
     public void grow() {
-
+        growing = true;
     }
 
     public boolean checkCollision() {
@@ -30,7 +46,13 @@ public class Snake {
 
     }
 
-    public void setDirection() {
+    public void setVelocity(int newDX, int newDY) {
+        if (dx == -newDX && dy == -newDY) {
+            return;
+        }
+
+        dx = newDX;
+        dy = newDY;
         
     }
 
